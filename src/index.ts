864d77c8591f68b787a54baeacd97f8b4da6ae5e
Nirintsoa <hhery889@gmail.com>
2021-12-29 22:51:10 +0300

    feat: setup inversifyService

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,30 @@
 import 'reflect-metadata'
 import dotenv from 'dotenv'
 import { createConnection } from 'typeorm'
-import express from 'express'
+import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 import { setupServer } from './tools/server'
+import { InversifyExpressServer } from 'inversify-express-utils'
+import container from './provider'
+import './controller'
 
 dotenv.config()
 
 createConnection()
     .then(() => {
-        const app = express()
-        app.use(cors())
-        app.use(express.json({ limit: '50mb' }))
-        app.use(express.urlencoded({ extended: true }))
+        const server = new InversifyExpressServer(container)
+
+        server.setConfig((app: Application) => {
+            app.use(cors())
+            app.use(express.json({ limit: '50mb' }))
+            app.use(express.urlencoded({ extended: true }))
+        })
+
+        const app = server.build()
+
+        app.use((req: Request, res: Response) => {
+            return res.status(404).send('404 NOT FOUND')
+        })
 
         setupServer(app)
     })
