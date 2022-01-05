7768da29f77a14576416372f2dbffb14f962df7a
Nirintsoa <hhery889@gmail.com>
2022-01-05 15:49:41 +0300

    fix: httpService return value

---
+++ src/Service/Http/HttpServiceType.ts
@@ -0,0 +1,10 @@
+import { Response } from "express";
+
+export interface IHttpService {
+    notFound: (res: Response, message?: string) => Response
+    badRequest: (res: Response, message?: string) => Response
+    errorServer: (res: Response, message?: string) => Response
+    unauthorized: (res: Response, message?: string) => Response
+    tokenExpired: (res: Response, message?: string) => Response
+    forbidden: (res: Response, message?: string) => Response
+}
