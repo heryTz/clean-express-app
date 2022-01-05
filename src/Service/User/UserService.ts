00ab0f23ecc055f1befa4fa930f2f54c1e6bc9e2
Nirintsoa <hhery889@gmail.com>
2022-01-05 23:20:10 +0300

    feat: User service

--- src/Service/User/UserService.ts
+++ src/Service/User/UserService.ts
@@ -2,6 +2,7 @@ import { injectable } from 'inversify'
 import { User } from '../../Entity/User'
 import { IUserService } from './UserServiceType'
 import jwt from 'jsonwebtoken'
+import { generate6Number } from '../../Tools/Helper'
 
 @injectable()
 export class UserService implements IUserService {
@@ -25,5 +26,17 @@ export class UserService implements IUserService {
             process.env.JWT_SECRET ?? '',
             { expiresIn: '24h' }
         )
+    }
+
+    generateCode() {
+        return String(generate6Number())
+    }
+
+    async sendPasswordCode(user: User) {
+        console.log(user)
+    }
+
+    async sendCodeActivation (user: User) {
+        console.log(user)
     }
 }
--- src/Service/User/UserServiceType.ts
+++ src/Service/User/UserServiceType.ts
@@ -3,4 +3,7 @@ import { User } from '../../Entity/User'
 export interface IUserService {
     generateToken: (user: User) => string
     generateRefreshToken: (user: User) => string
+    generateCode: () => string
+    sendCodeActivation: (user: User) => void
+    sendPasswordCode: (user: User) => void
 }
